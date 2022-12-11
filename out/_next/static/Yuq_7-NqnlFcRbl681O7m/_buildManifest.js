dd869b02e1c3ea55ca5c350b00d7dd8b4a0078c0
Nemanja Janicijevic <neca7599@gmail.com>
2022-12-11 21:25:53 +0100

    Delete out directory

--- out/_next/static/Yuq_7-NqnlFcRbl681O7m/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-a0acaabbd60b96da.js"],"/_error":["static/chunks/pages/_error-0509152792d2b207.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
